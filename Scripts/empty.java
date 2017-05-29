f0033e8efd8b1d8fd52bd4fcb0aed47a3637153e
Torrey Leonard <torreyleonard@voxela.com>
2017-05-29 00:15:38 -0400

    Create empty.java

---
+++ Scripts/empty.java
@@ -0,0 +1,4 @@
+// This isn't a script, so don't try running it.
+// If you have some ideas for what can be added to this default script library, please let me know!
+// Post it on the plugin's discussion thread: http://voxe.la/javashell
+// Thanks again for using JavaShell!
